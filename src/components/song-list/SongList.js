ce2d8958902ae4b33d0c35e6dc4e2f36646cb659
adarshkumar714 <adarshkumar20012704@gmail.com>
2023-04-20 19:24:36 +0530

    testing song playing

--- src/components/song-list/SongList.js
+++ src/components/song-list/SongList.js
@@ -1,55 +1,62 @@
 import './SongList.css'
+import { useState } from 'react';
 
 export default function SongList(){
     return (
         <>
             <div className='song-list'>
-                <SongItem songIcon='./saved-playlist-icon-2.png' songName='Illegal Weapon' singer='Tanishk B,Jasmine S,Garry S' isPlaying={false}/>
+                <SongItem songIcon='./saved-playlist-icon-2.png' songName='Illegal Weapon' singer='Tanishk B,Jasmine S,Garry S' />
                 <div className='song-item-breaker'></div>
-                <SongItem songIcon='./saved-playlist-icon-2.png' songName='Illegal Weapon' singer='Tanishk B,Jasmine S,Garry S' isPlaying={false}/>
+                <SongItem songIcon='./saved-playlist-icon-2.png' songName='Illegal Weapon' singer='Tanishk B,Jasmine S,Garry S' />
                 <div className='song-item-breaker'></div>
-                <SongItem songIcon='./saved-playlist-icon-2.png' songName='Illegal Weapon' singer='Tanishk B,Jasmine S,Garry S' isPlaying={false}/>
+                <SongItem songIcon='./saved-playlist-icon-2.png' songName='Illegal Weapon' singer='Tanishk B,Jasmine S,Garry S' />
                 <div className='song-item-breaker'></div>
-                <SongItem songIcon='./saved-playlist-icon-2.png' songName='Illegal Weapon' singer='Tanishk B,Jasmine S,Garry S' isPlaying={false}/>
+                <SongItem songIcon='./saved-playlist-icon-2.png' songName='Illegal Weapon' singer='Tanishk B,Jasmine S,Garry S' />
                 <div className='song-item-breaker'></div>
-                <SongItem songIcon='./saved-playlist-icon-2.png' songName='Illegal Weapon' singer='Tanishk B,Jasmine S,Garry S' isPlaying={false}/>
+                <SongItem songIcon='./saved-playlist-icon-2.png' songName='Illegal Weapon' singer='Tanishk B,Jasmine S,Garry S' />
                 <div className='song-item-breaker'></div>
-                <SongItem songIcon='./saved-playlist-icon-2.png' songName='Illegal Weapon' singer='Tanishk B,Jasmine S,Garry S' isPlaying={false}/>
+                <SongItem songIcon='./saved-playlist-icon-2.png' songName='Illegal Weapon' singer='Tanishk B,Jasmine S,Garry S' />
                 <div className='song-item-breaker'></div>
-                <SongItem songIcon='./saved-playlist-icon-2.png' songName='Illegal Weapon' singer='Tanishk B,Jasmine S,Garry S' isPlaying={false}/>
+                <SongItem songIcon='./saved-playlist-icon-2.png' songName='Illegal Weapon' singer='Tanishk B,Jasmine S,Garry S' />
                 <div className='song-item-breaker'></div>
-                <SongItem songIcon='./saved-playlist-icon-2.png' songName='Illegal Weapon' singer='Tanishk B,Jasmine S,Garry S' isPlaying={false}/>
+                <SongItem songIcon='./saved-playlist-icon-2.png' songName='Illegal Weapon' singer='Tanishk B,Jasmine S,Garry S' />
                 <div className='song-item-breaker'></div>
-                <SongItem songIcon='./saved-playlist-icon-2.png' songName='Illegal Weapon' singer='Tanishk B,Jasmine S,Garry S' isPlaying={false}/>
+                <SongItem songIcon='./saved-playlist-icon-2.png' songName='Illegal Weapon' singer='Tanishk B,Jasmine S,Garry S' />
                 <div className='song-item-breaker'></div>
-                <SongItem songIcon='./saved-playlist-icon-2.png' songName='Illegal Weapon' singer='Tanishk B,Jasmine S,Garry S' isPlaying={false}/>
+                <SongItem songIcon='./saved-playlist-icon-2.png' songName='Illegal Weapon' singer='Tanishk B,Jasmine S,Garry S' />
                 <div className='song-item-breaker'></div>
-                <SongItem songIcon='./saved-playlist-icon-2.png' songName='Illegal Weapon' singer='Tanishk B,Jasmine S,Garry S' isPlaying={false}/>
+                <SongItem songIcon='./saved-playlist-icon-2.png' songName='Illegal Weapon' singer='Tanishk B,Jasmine S,Garry S' />
                 <div className='song-item-breaker'></div>
-                <SongItem songIcon='./saved-playlist-icon-2.png' songName='Illegal Weapon' singer='Tanishk B,Jasmine S,Garry S' isPlaying={false}/>
+                <SongItem songIcon='./saved-playlist-icon-2.png' songName='Illegal Weapon' singer='Tanishk B,Jasmine S,Garry S' />
                 <div className='song-item-breaker'></div>
-                <SongItem songIcon='./saved-playlist-icon-2.png' songName='Illegal Weapon' singer='Tanishk B,Jasmine S,Garry S' isPlaying={false}/>
+                <SongItem songIcon='./saved-playlist-icon-2.png' songName='Illegal Weapon' singer='Tanishk B,Jasmine S,Garry S' />
                 <div className='song-item-breaker'></div>
-                <SongItem songIcon='./saved-playlist-icon-2.png' songName='Illegal Weapon' singer='Tanishk B,Jasmine S,Garry S' isPlaying={false}/>
+                <SongItem songIcon='./saved-playlist-icon-2.png' songName='Illegal Weapon' singer='Tanishk B,Jasmine S,Garry S' />
                 <div className='song-item-breaker'></div>
-                <SongItem songIcon='./saved-playlist-icon-2.png' songName='Illegal Weapon' singer='Tanishk B,Jasmine S,Garry S' isPlaying={false}/>
+                <SongItem songIcon='./saved-playlist-icon-2.png' songName='Illegal Weapon' singer='Tanishk B,Jasmine S,Garry S' />
                 <div className='song-item-breaker'></div>
-                <SongItem songIcon='./saved-playlist-icon-2.png' songName='Illegal Weapon' singer='Tanishk B,Jasmine S,Garry S' isPlaying={false}/>
+                <SongItem songIcon='./saved-playlist-icon-2.png' songName='Illegal Weapon' singer='Tanishk B,Jasmine S,Garry S' />
             </div>
         </>
     );
 }
 
 function SongItem(props){
+    const [isPlaying, setIsPlaying] = useState(false);
+    
     return (
         <>
-            <div className='song-item'>
+            <div className='song-item' onClick={songClickHandler}>
                 <img className='song-item-icon' src={props.songIcon} alt='song-item-icon'></img>
                 <div className='song-item-name'>
-                    <div className='song-songname'>{props.songName}</div>
-                    <div className='song-singer'>{props.singer}</div>
+                    <div className='song-songname' style={isPlaying?({'color':'#c300c3'}):({})}>{props.songName}</div>
+                    <div className='song-singer' style={isPlaying?({'color':'#396de3'}):({})}>{props.singer}</div>
                 </div>
             </div>
         </>
     );
+
+    function songClickHandler(){
+        setIsPlaying(!isPlaying);
+    }
 }
