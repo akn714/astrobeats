d0a9e98f1595e897250217c22feb4f82a64f8b28
adarshkumar714 <123639529+adarshkumar714@users.noreply.github.com>
2023-04-22 15:55:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,38 +5,50 @@ import Library from './components/library-section/Library'
 import SongList from './components/song-list/SongList'
 import Navigation from './components/navigation/Navigation'
 
-// const router = createBrowserRouter(
-//   createRoutesFromElements(
-//     <Route path="/" element={<Navigation element={<Home />} />}>
-//       <Route path="library" element={<Navigation element={<Library />} />} />
-//       <Route
-//         path="home"
-//         element={<Navigation element={<Home />} />}
-//       />
-//       <Route
-//         path="login"
-//         element={<Navigation element={<Login />} />}
-//       />
-//     </Route>
-//   )
-// );
+import {
+   createBrowserRouter,
+   createRoutesFromElements,
+   RouterProvider,
+   Route,
+   Link,
+ } from "react-router-dom";
+
+ const router = createBrowserRouter(
+   createRoutesFromElements(
+     <Route path="/">
+       <Route
+        path=""
+        element={<Home />}
+       />
+       <Route
+         path="login"
+        element={<Login />}
+       />
+       <Route
+        path="library"
+        element={<Library />}
+       />
+     </Route>
+   )
+);
 
 // createRoot(document.getElementById("root")).render(
 //   <RouterProvider router={router} />
 // );
 
 function App() {
-  return (
-    <div className="body">
-        {/* <div>Welcome to AstroBeats!</div> */}
-        {/* <Login /> */}
-        {/* <Home /> */}
-        {/* <Library /> */}
-        {/* <SongList /> */}
-        <Navigation element={<SongList />} />
-        {/* <Navigation /> */}
-    </div>
-  );
+    return (
+        <div className="body">
+            {/* <div>Welcome to AstroBeats!</div> */}
+            {/* <Login /> */}
+            {/* <Home /> */}
+            {/* <Library /> */}
+            {/* <SongList /> */}
+            <Navigation element={<RouterProvider router={router} />} />
+            
+            {/* <Navigation /> */}
+        </div>
+    );
 }
 
 export default App;
