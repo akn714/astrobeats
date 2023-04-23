aebea6607d6e32af1ffe60e19807fce61afc27ec
adarshkumar714 <123639529+adarshkumar714@users.noreply.github.com>
2023-04-23 10:15:19 +0530

    Changing default export to named export

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css'
-import Login from './components/Login/Login'
-import Home from './components/home-section/Home'
-import Library from './components/library-section/Library'
-import SongList from './components/song-list/SongList'
-import Navigation from './components/navigation/Navigation'
+import { Login } from './components/Login/Login'
+import { Home } from './components/home-section/Home'
+import { Library } from './components/library-section/Library'
+import { SongList } from './components/song-list/SongList'
+import { Navigation } from './components/navigation/Navigation'
 
 import {
    createBrowserRouter,
