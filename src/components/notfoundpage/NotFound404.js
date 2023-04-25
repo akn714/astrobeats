27377f73232c197ded4d132198eacfc5735402a0
adarshkumar714 <adarshkumar20012704@gmail.com>
2023-04-25 18:04:27 +0530

    updated not found page

--- src/components/notfoundpage/NotFound404.js
+++ src/components/notfoundpage/NotFound404.js
@@ -1,10 +1,12 @@
 import './NotFound404.css'
+import { Link } from 'react-router-dom'
 
 export function NotFound404() {
     return (
         <>
             <div class="page-not-found">
-                page not found
+                <h2>404 page not found !</h2>
+                <p>go to <Link to='/'>home</Link> instead.</p>
             </div>
         </>
     );
