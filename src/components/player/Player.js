39e6df1930878410452bafd36058ba644ddd6d2d
adarshkumar714 <adarshkumar20012704@gmail.com>
2023-04-26 20:43:01 +0530

    updated player

--- src/components/player/Player.js
+++ src/components/player/Player.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './Player.css'
 
 import {
@@ -5,9 +6,16 @@ import {
  } from "react-router-dom";
 
 export function Player(props){
+
+    const [song, setSong] = useState({
+       songname: null,
+       artist: null 
+    });
+
     return (
         <>
             <div className='player'>
+	            <audio id="audio" type="audio/mp3" src="./test-song/Illegal Weapon 2.0 - Street Dancer 3D 128 Kbps.mp3"></audio>
                 <div className='player-top-row'>
                     <div className='player-song'>
                         <img className='player-song-icon' src='./saved-playlist-icon-2.png' alt={`${props.songname}`}/>
@@ -17,25 +25,45 @@ export function Player(props){
                         </div>
                     </div>
                     <div className='player-top-row-controllers'>
-                        <img className='player-top-row-controllers-big' src='./player icons/prev-next.png' alt='prev' style={{'rotate':'180deg'}} />
-                        <img src='./player icons/backward-10s.png' alt='backward-10s' />
-                        <img className='player-top-row-controllers-big play-pause-btn' onClick={PlayingStateHandler} src='./player icons/play.png' alt='play' />
-                        <img src='./player icons/forward-10s.png' alt='forward-10s.png' />
-                        <img className='player-top-row-controllers-big' src='./player icons/prev-next.png' alt='next' />
+                        <div className='player-top-row-controllers-upper'>
+                            <img className='player-top-row-controllers-big' src='./player icons/prev-next.png' alt='prev' style={{'rotate':'180deg'}} />
+                            <img src='./player icons/backward-10s.png' alt='backward-10s' onClick={()=>{document.getElementById('audio').currentTime=document.getElementById('audio').currentTime-10}} />
+                            <img className='player-top-row-controllers-big play-pause-btn' onClick={PlayingStateHandler} src='./player icons/play.png' alt='play' />
+                            <img src='./player icons/forward-10s.png' alt='forward-10s.png' onClick={()=>{document.getElementById('audio').currentTime=document.getElementById('audio').currentTime+10}} />
+                            <img className='player-top-row-controllers-big' src='./player icons/prev-next.png' alt='next' />
+                        </div>
+                        <div className='player-top-row-controllers-volume'>
+                            <input type='range' className='player-top-row-controllers-volume-btn' min='0' max='100' onChange={()=>{
+                                document.getElementById('audio').volume = document.getElementsByClassName('player-top-row-controllers-volume-btn')[0].value/100;
+                            }} />
+                        </div>
                     </div>
                 </div>
                 <div className='player-controller'>
-                    <input type='range' />
+                    <input type='range' className='player-controller-range' min='0' max='188.656327' onChange={()=>{
+        if(!audio.paused){
+            document.getElementsByClassName('play-pause-btn')[0].src='./player icons/pause.png';
+            audio.play();
+        }
+        else{
+            document.getElementsByClassName('play-pause-btn')[0].src='./player icons/play.png';
+            audio.pause();
+        }
+                        document.getElementById('audio').currentTime = document.getElementsByClassName('player-controller-range')[0].value;
+                    }}/>
                 </div>
             </div>
         </>
     );
 
     function PlayingStateHandler(){
-        if(document.getElementsByClassName('play-pause-btn')[0].src==`${window.location.href}player%20icons/play.png`){
+        let audio = document.getElementById('audio');
+        if(audio.paused){
             document.getElementsByClassName('play-pause-btn')[0].src='./player icons/pause.png';
+            audio.play();
             return;
         }
         document.getElementsByClassName('play-pause-btn')[0].src='./player icons/play.png';
+        audio.pause();
     }
 }
