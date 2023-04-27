8856f3268d933b9263135c6ca272ee284d047027
adarshkumar714 <adarshkumar20012704@gmail.com>
2023-04-27 20:00:16 +0530

    working on song playing and SongList

--- src/components/player/Player.js
+++ src/components/player/Player.js
@@ -15,7 +15,7 @@ export function Player(props){
     return (
         <>
             <div className='player'>
-	            <audio id="audio" type="audio/mp3" src="./test-song/Illegal Weapon 2.0 - Street Dancer 3D 128 Kbps.mp3"></audio>
+	            {/* <audio id='song' type="audio/mp3" src="./test-song/Illegal Weapon 2.0 - Street Dancer 3D 128 Kbps.mp3"></audio> */}
                 <div className='player-top-row'>
                     <div className='player-song'>
                         <img className='player-song-icon' src='./saved-playlist-icon-2.png' alt={`${props.songname}`}/>
@@ -27,14 +27,14 @@ export function Player(props){
                     <div className='player-top-row-controllers'>
                         <div className='player-top-row-controllers-upper'>
                             <img className='player-top-row-controllers-big' src='./player icons/prev-next.png' alt='prev' style={{'rotate':'180deg'}} />
-                            <img src='./player icons/backward-10s.png' alt='backward-10s' onClick={()=>{document.getElementById('audio').currentTime=document.getElementById('audio').currentTime-10}} />
+                            <img src='./player icons/backward-10s.png' alt='backward-10s' onClick={()=>{document.getElementById('song').currentTime=document.getElementById('song').currentTime-10}} />
                             <img className='player-top-row-controllers-big play-pause-btn' onClick={PlayingStateHandler} src='./player icons/play.png' alt='play' />
-                            <img src='./player icons/forward-10s.png' alt='forward-10s.png' onClick={()=>{document.getElementById('audio').currentTime=document.getElementById('audio').currentTime+10}} />
+                            <img src='./player icons/forward-10s.png' alt='forward-10s.png' onClick={()=>{document.getElementById('song').currentTime=document.getElementById('song').currentTime+10}} />
                             <img className='player-top-row-controllers-big' src='./player icons/prev-next.png' alt='next' />
                         </div>
                         <div className='player-top-row-controllers-volume'>
                             <input type='range' className='player-top-row-controllers-volume-btn' min='0' max='100' onChange={()=>{
-                                document.getElementById('audio').volume = document.getElementsByClassName('player-top-row-controllers-volume-btn')[0].value/100;
+                                document.getElementById('song').volume = document.getElementsByClassName('player-top-row-controllers-volume-btn')[0].value/100;
                             }} />
                         </div>
                     </div>
@@ -49,7 +49,7 @@ export function Player(props){
             document.getElementsByClassName('play-pause-btn')[0].src='./player icons/play.png';
             audio.pause();
         }
-                        document.getElementById('audio').currentTime = document.getElementsByClassName('player-controller-range')[0].value;
+                        document.getElementById('song').currentTime = document.getElementsByClassName('player-controller-range')[0].value;
                     }}/>
                 </div>
             </div>
@@ -57,13 +57,13 @@ export function Player(props){
     );
 
     function PlayingStateHandler(){
-        let audio = document.getElementById('audio');
-        if(audio.paused){
+        let song = document.getElementById('song');
+        if(song.paused){
             document.getElementsByClassName('play-pause-btn')[0].src='./player icons/pause.png';
-            audio.play();
+            song.play();
             return;
         }
         document.getElementsByClassName('play-pause-btn')[0].src='./player icons/play.png';
-        audio.pause();
+        song.songpause();
     }
 }
--- src/components/song-list/SongList.js
+++ src/components/song-list/SongList.js
@@ -1,10 +1,35 @@
 import './SongList.css'
 import { useState } from 'react';
 
+import { songs } from './songsData';
+
 export function SongList() {
+
+    const [Song, setSong] = useState({
+        key: 1,
+        songname: '2U',
+        artist: 'Justin Bieber',
+        src: './test-song/2U - Justin Bieber.mp3'
+    })
+
     return (
         <>
             <div className='song-list'>
+                <audio id={`song#${Song.key}`} type="audio/mp3" src={`${Song.src}`} />
+
+                {
+                    songs.map((song)=>{
+                        {
+                            return (
+                                <>
+                                    <SongItem key={song.id} songIcon='./saved-playlist-icon-2.png' song={song} />
+                                    <div className='song-item-breaker'></div>
+                                </>
+                            )
+                        }
+                    })
+                }
+{/* 
                 <SongItem songIcon='./saved-playlist-icon-2.png' songName='Illegal Weapon' artist='Tanishk B,Jasmine S,Garry S' />
                 <div className='song-item-breaker'></div>
                 <SongItem songIcon='./saved-playlist-icon-2.png' songName='Illegal Weapon' artist='Tanishk B,Jasmine S,Garry S' />
@@ -31,28 +56,33 @@ export function SongList() {
                 <div className='song-item-breaker'></div>
                 <SongItem songIcon='./saved-playlist-icon-2.png' songName='Illegal Weapon' artist='Tanishk B,Jasmine S,Garry S' />
                 <div className='song-item-breaker'></div>
-                <SongItem songIcon='./saved-playlist-icon-2.png' songName='Illegal Weapon' artist='Tanishk B,Jasmine S,Garry S' />
+                <SongItem songIcon='./saved-playlist-icon-2.png' songName='Illegal Weapon' artist='Tanishk B,Jasmine S,Garry S' /> */}
             </div>
         </>
     );
-}
 
-function SongItem(props) {
-    const [isPlaying, setIsPlaying] = useState(false);
 
-    return (
-        <>
-            <div className='song-item' onClick={songClickHandler}>
-                <img className='song-item-icon' src={props.songIcon} alt='song-item-icon'></img>
-                <div className='song-item-name'>
-                    <div className='song-songname' style={isPlaying ? ({ 'color': '#c300c3' }) : ({})}>{props.songName}</div>
-                    <div className='song-singer' style={isPlaying ? ({ 'color': '#396de3' }) : ({})}>{props.artist}</div>
+    
+    function SongItem(props) {
+        const [isPlaying, setIsPlaying] = useState(false);
+
+        return (
+            <>
+                <div className='song-item' onClick={(song)=>{songClickHandler(props.song)}}>
+                    <img className='song-item-icon' src={props.songIcon} alt='song-item-icon'></img>
+                    <div className='song-item-name'>
+                        <div className='song-songname' style={isPlaying ? ({ 'color': '#c300c3' }) : ({})}>{props.song.songname}</div>
+                        <div className='song-singer' style={isPlaying ? ({ 'color': '#396de3' }) : ({})}>{props.song.artist}</div>
+                    </div>
                 </div>
-            </div>
-        </>
-    );
+            </>
+        );
 
-    function songClickHandler() {
-        setIsPlaying(!isPlaying);
+        function songClickHandler(song) {
+            setIsPlaying(!isPlaying);
+            document.getElementById(`song#${Song.key}`).pause();
+            setSong(song);
+            document.getElementById(`song#${Song.key}`).play();
+        }
     }
 }
