dca7f2e42133dd86b362fadf629c33dc66c1ed6d
adarshkumar714 <123639529+adarshkumar714@users.noreply.github.com>
2023-04-28 20:14:17 +0530

    Update Player.js

--- src/components/player/Player.js
+++ src/components/player/Player.js
@@ -16,7 +16,8 @@ export function Player(props) {
     return (
         <>
             <div className='player'>
-                <audio controls id='song' type="audio/mp3" src={null}></audio>
+                {/* removed controls */}
+                <audio id='song' type="audio/mp3" src={null}></audio>
                 <div className='player-top-row'>
                     <div className='player-song'>
                         <img className='player-song-icon' src='./saved-playlist-icon-2.png' alt={`${props.songname}`} />
@@ -29,11 +30,9 @@ export function Player(props) {
                         <div className='player-top-row-controllers-upper'>
 
                             <img className='player-top-row-controllers-big' src='./player icons/prev-next.png' alt='prev' style={{ 'rotate': '180deg' }} />
-                            <img src='./player icons/backward-10s.png' alt='backward-10s' onClick={() => { document.getElementById('song').currentTime = document.getElementById('song').currentTime - 10 }} />
+                            <img src='./player icons/backward-10s.png' alt='backward-10s' onClick={() => { document.getElementById('song').currentTime -= 10 }} />
                             <img className='player-top-row-controllers-big play-pause-btn' onClick={PlayingStateHandler} src='./player icons/play.png' alt='play' />
-                            <img src='./player icons/forward-10s.png' alt='forward-10s.png' onClick={() => {
-                                document.getElementById('song').currentTime += 100
-                            }} />
+                            <img src='./player icons/forward-10s.png' alt='forward-10s.png' onClick={() => { document.getElementById('song').currentTime += 10 }} />
                             <img className='player-top-row-controllers-big' src='./player icons/prev-next.png' alt='next' />
                         </div>
                         <div className='player-top-row-controllers-volume'>
@@ -45,13 +44,14 @@ export function Player(props) {
                 </div>
                 <div className='player-controller'>
                     {
-                        setInterval(()=>{
-                            document.getElementsByClassName('player-controller-range')[0].value = document.getElementById('song').currentTime*10
-                            console.log(document.getElementsByClassName('player-controller-range')[0].value, document.getElementById('song').currentTime)
-                        },1000)
+                        setInterval(() => {
+                            document.getElementsByClassName('player-controller-range')[0].value = document.getElementById('song').currentTime * 10
+                            // console.log(document.getElementsByClassName('player-controller-range')[0].value, document.getElementById('song').currentTime)
+                        }, 1000)
                     }
-                    <input type='range' className='player-controller-range' value='0' onChange={() => {
+                    <input type='range' className='player-controller-range' onChange={() => {
                         let song = document.getElementById('song');
+                        document.getElementsByClassName('player-controller-range')[0].setAttribute('max', song.duration * 10);
                         if (!song.paused) {
                             document.getElementsByClassName('play-pause-btn')[0].src = './player icons/pause.png';
                             song.play();
@@ -60,7 +60,7 @@ export function Player(props) {
                             document.getElementsByClassName('play-pause-btn')[0].src = './player icons/play.png';
                             song.pause();
                         }
-                        document.getElementById('song').currentTime = document.getElementsByClassName('player-controller-range')[0].value/10;
+                        document.getElementById('song').currentTime = document.getElementsByClassName('player-controller-range')[0].value / 10;
                     }} />
                 </div>
             </div>
