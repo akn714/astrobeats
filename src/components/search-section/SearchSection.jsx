864dde87427ff190fe7efce144043f09997ffbc7
adarshkumar714 <adarshkumar20012704@gmail.com>
2023-05-22 18:29:14 +0530

    developing search-section

--- src/components/search-section/SearchSection.jsx
+++ src/components/search-section/SearchSection.jsx
@@ -1,5 +1,13 @@
 export function SearchSection(){
     return (<>
-        <input type='text' placeholder='search' />
+        <div className="search">
+            <input type='text' placeholder='search' />
+            <div className="search-results">
+                <div className="search-message">
+                    <h1>Search here!</h1>
+                    <p>search song, albums, artists and many more...</p>
+                </div>
+            </div>
+        </div>
     </>);
 }
