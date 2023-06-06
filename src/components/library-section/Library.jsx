f202a0c739305a25b9596c9547f5ae383706ed56
adarshkumar714 <adarshkumar20012704@gmail.com>
2023-06-06 18:25:25 +0530

    improved search bar of library section

--- src/components/library-section/Library.jsx
+++ src/components/library-section/Library.jsx
@@ -10,6 +10,9 @@ export function Library() {
             <div className='library-playlists'>
                 <div className='library-playlists-search'>
                     <input type='text'  placeholder='search playlist' />
+                    <button>
+                        <img src="./nav icons/search.png" alt="" />
+                    </button>
                 </div>
                 <SavedPlaylist playlistName='playlist 1' playlistIcon='./saved-playlist-icon-2.png' />
                 <SavedPlaylist playlistName='playlist 2' playlistIcon='./saved-playlist-icon-2.png' />
