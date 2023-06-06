4eb27acf6d1643b3da483830c8425369d5a76256
adarshkumar714 <adarshkumar20012704@gmail.com>
2023-06-06 18:10:56 +0530

    minor changes in search section, started developing settings section

--- src/components/search-section/SearchSection.jsx
+++ src/components/search-section/SearchSection.jsx
@@ -2,17 +2,19 @@ import './SearchSection.css'
 
 export function SearchSection(){
     return (<>
-        <div className="search">
-            <div className="search-bar">
-                <input type='text' placeholder='search' />
-                <button>
-                    <img src="./nav icons/search.png" alt="" />
-                </button>
-            </div>
-            <div className="search-results">
-                <div className="search-message">
-                    <h1>Search here!</h1>
-                    <p>search song, albums, artists and many more...</p>
+        <div className="search-section">
+            <div className='search-main'>
+                <div className="search-bar">
+                    <input type='text' placeholder='search' />
+                    <button>
+                        <img src="./nav icons/search.png" alt="" />
+                    </button>
+                </div>
+                <div className="search-results">
+                    <div className="search-message">
+                        <h1>Search here!</h1>
+                        <p>search song, albums, artists and many more...</p>
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/settings/Settings.jsx
+++ src/components/settings/Settings.jsx
@@ -2,8 +2,8 @@ import './Settings.css'
 
 export function Settings(){
     return (
-        <>
+        <div className='settings-section'>
             <h1>this is settings section</h1>
-        </>
+        </div>
     );
 }
