b2aec93e553d69748254cec39bd949f3f086a66a
adarshkumar714 <adarshkumar20012704@gmail.com>
2023-06-16 23:39:00 +0530

    Revert "Revert "improved seek-bar and added file of options""
    This reverts commit 4197fbba672b4cf6de370298e8cce0f4e9f42c77.

---
+++ src/components/song-list/songOptions.jsx
@@ -0,0 +1,10 @@
+export function songOptions(){
+    return (
+        <>
+            <div className="song-options">
+                <button className="add-to-playlist">add to playlist</button>
+                <button className="remove-from-playlist">remove from playlist</button>
+            </div>
+        </>
+    );
+}
